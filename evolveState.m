eb4955c88759fe1487e2d18c6c85d0dbc36da54d
Kenneth Foead <kennethubert17@gmail.com>
2024-11-23 16:33:14 -0800

    Update/rename some existing files and create deadIndex.m

--- evolveState.m
+++ evolveState.m
@@ -1,10 +1,13 @@
 function evolveState(R,C,m,n)
 
-% Check for surviving cells
+% Translate R and C into cells
+coordinate = translateCoordinate(R,C);
 
+% Check for surviving cells
+surviveN = surviveIndex(coordinate);
 
 % Check for dying cells
-
+deadN = deadIndex(coordinate);
 
 % Check for awaking cells
 
---
+++ surviveIndex.m
@@ -0,0 +1,23 @@
+function index = surviveIndex(coord)
+
+index = []; % Store n-th cell survive
+
+len = length(coord); % Count the number of living cells
+
+% Check for number of neighbours
+for i = 1:len
+    count = 0;
+    for j = -1:1
+        for k = -1:1
+            if ~isempty(locateCoordinate(coord, [coord{i}(1)+j, coord{i}(2)+k]))
+                count = count + 1;
+            end
+        end
+    end
+    count = count - 1;
+    if count == 2 || count == 3
+        index = [index i];
+    end
+end
+         
+end
