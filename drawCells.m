c76b6b1407d760ec9de7d46749ef30681c8d91e8
fz600 <iwin@ucsd.edu>
2024-11-30 00:18:33 -0800

    drawCells.m
    WIll need to be updated each time

--- drawCells.m
+++ drawCells.m
@@ -1,6 +1,5 @@
 function drawCells(m,n)
 
-figure;
 hold on;
 
 axis([0 m 0 n]);
@@ -9,10 +8,10 @@ axis equal;
 axis off;
 
 for i=0:m
-    plot([i i], [0 n],'Color',[.7 .7 .7], 'linewidth', 2);
+    plot([i i], [0 n],'Color',[.7 .7 .7], 'linewidth', 3);
 end
 for j=0:n
-    plot([0 m], [j j],'Color', [.7 .7 .7], 'linewidth', 2);
+    plot([0 m], [j j],'Color', [.7 .7 .7], 'linewidth', 3);
 end
 end
 
