4ef4d13bcff5fc20b9b44ab8953611755b8bf690
Kenneth Foead <kennethubert17@gmail.com>
2024-11-30 12:02:40 -0800

    Update updateCellFig.m: clear everything when it is called and draw grid at the end

--- updateCellFig.m
+++ updateCellFig.m
@@ -1,12 +1,15 @@
-function updateCellFig(R,C)
+function updateCellFig(R,C,m,n)
+
+cla;
 hold on
 
-for i=1:Length(R)
+for i=1:length(R)
     R_value = R(i);
     Y = [R_value - 1, R_value, R_value, R_value - 1];
     C_value = C(i);
     X = [C_value-1, C_value-1, C_value, C_value];
 
     fill(X,Y,'k')
+    drawCells(m,n)
 end
 end
