5a551268777682019e5d5c1bcf243c6f338ed815
Marcel <mdekerpel@ucsd.edu>
2024-11-30 22:42:16 -0800

    Marcel De Kerpel - I worked on the inputLiveCells function

---
+++ inputLiveCells.m
@@ -0,0 +1,29 @@
+function [R, C] = inputLiveCells(m, n)
+
+drawCells(m, n);
+
+title('Click on squares, press Enter to finish.')
+
+R = [];
+C = [];
+
+while true
+[x, y, button] = ginput(1);
+if isempty(button) || button == 13
+    title('');
+    break
+end
+
+newR = floor(y) + 1;
+newC = floor(x) + 1;
+
+R = [R; newR];
+C = [C; newC];
+
+updateCellFig(R,C,m,n)
+end
+
+R = transpose(R);
+C = transpose(C);
+
+end
