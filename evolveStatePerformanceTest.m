b45d32570f254d718a4de4c579a3f47f419c1c20
Kenneth Foead <kennethubert17@gmail.com>
2024-12-02 01:41:34 -0800

    Update testing file

--- evolveStatePerformanceTest.m
+++ evolveStatePerformanceTest.m
@@ -1,7 +1,7 @@
 tic;
 generation = 0; m = 15; n = 15;
-R = [2 2 3 4 4 4 3 5 4 12 11 11 11];
-C = [3 4 5 5 6 7 7 6 8 12 12 13 14];
+R = [9 9 10 8 8];
+C = [7 8 8 8 9];
 updateCellFig(R,C,m,n,generation)
 pause(0.01)
 generation = generation + 1;
@@ -10,7 +10,7 @@ generation = generation + 1;
 [Rnew, Cnew] = evolveState(R,C,m,n,generation); %get pattern after the first tick
 pause(0.01)
 generation = generation + 1;
-while ~isequal([R; C],[Rnew; Cnew]) %as long as pattern is changing following a tick
+while generation <= 30 %as long as pattern is changing following a tick
     R = Rnew; C = Cnew; %pass updated information through
     [Rnew, Cnew] = evolveState(R,C,m,n,generation); %obtain new pattern after one tick
     generation = generation + 1;
